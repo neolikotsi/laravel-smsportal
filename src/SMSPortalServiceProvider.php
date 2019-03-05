615316febee29ac86d4faa1f03d2b045a3bc4fe0
bitbuket.org/sleepsolutions <neo@sleepsolutions-sa.com>
2019-03-05 15:03:09 +0200

    update service provider

--- src/SMSPortalServiceProvider.php
+++ src/SMSPortalServiceProvider.php
@@ -6,6 +6,18 @@ use Illuminate\Support\ServiceProvider;
 
 class SMSPortalServiceProvider extends ServiceProvider
 {
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/smsportal.php' => config_path('smsportal.php')
+        ], 'config');
+    }
+
     public function register()
     {
         Notification::extend('smsportal', function ($app) {
