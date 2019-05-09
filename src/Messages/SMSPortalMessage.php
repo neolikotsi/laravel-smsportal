6bc82198e12b6b90d70eb6a03ed044e4970edd9d
bitbuket.org/sleepsolutions <neo@sleepsolutions-sa.com>
2019-05-09 13:36:40 +0200

    update php-smsportal package

--- src/Messages/SMSPortalMessage.php
+++ src/Messages/SMSPortalMessage.php
@@ -2,38 +2,12 @@
 
 namespace Illuminate\Notifications\Messages;
 
-class SMSPortalMessage
+use NeoLikotsi\SMSPortal\Message;
+
+class SMSPortalMessage extends Message
 {
-    public const LINE_BREAK = '|';
-
-    /**
-     * The message content.
-     *
-     * @var string
-     */
-    public $content;
-
-    /**
-     * Create a new message instance.
-     *
-     * @param  string  $content
-     * @return void
-     */
     public function __construct(string $content = '')
     {
-        $this->content($content);
-    }
-
-    /**
-     * Set the message content.
-     *
-     * @param  string  $content
-     * @return $this
-     */
-    public function content(string $content)
-    {
-        $this->content = str_replace('<br>', '|', nl2br($content, false));
-
-        return $this;
+        parent::__construct($content);
     }
 }
